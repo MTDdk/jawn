41927fcf242200aa94408bcae575ce2c3ff1f3c2
MTD <alvn@alvn.dk>
2015-05-28 11:46:23 +0200

    feat(database): creating pooled datasource

--- jawn-core/src/main/java/net/javapla/jawn/core/database/DatabaseConnection.java
+++ jawn-core/src/main/java/net/javapla/jawn/core/database/DatabaseConnection.java
@@ -1,8 +1,23 @@
 package net.javapla.jawn.core.database;
 
+import java.beans.PropertyVetoException;
+
+import javax.sql.DataSource;
+
 public interface DatabaseConnection {
     String url();
     String driver();
     String user();
     String password();
+    
+    int maxPoolSize();
+    int minPoolSize();
+    
+    /**
+     * 
+     * @return
+     * @throws ClassNotFoundException If the driver was not found
+     * @throws PropertyVetoException If any of the input is unacceptable
+     */
+    DataSource createDataSource() throws ClassNotFoundException, PropertyVetoException;
 }
--- jawn-core/src/main/java/net/javapla/jawn/core/database/DatabaseConnections.java
+++ jawn-core/src/main/java/net/javapla/jawn/core/database/DatabaseConnections.java
@@ -1,9 +1,14 @@
 package net.javapla.jawn.core.database;
 
+import java.beans.PropertyVetoException;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.sql.DataSource;
+
 import net.javapla.jawn.core.util.Modes;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class DatabaseConnections {
 
@@ -32,6 +37,9 @@ public class DatabaseConnections {
         JdbcConnectionBuilder driver(String driver);
         JdbcConnectionBuilder user(String user);
         JdbcConnectionBuilder password(String pass);
+        
+        JdbcConnectionBuilder maxPoolSize(int max);
+        JdbcConnectionBuilder minPoolSize(int min);
     }
     
     public class DatabaseConnectionBuilderImpl implements DatabaseConnectionBuilder, DatabaseConnection {
@@ -40,26 +48,60 @@ public class DatabaseConnections {
         String url;
         String user;
         String password;
+        int maxPoolSize = 8;
+        int minPoolSize = 1;
         
         public DatabaseConnectionBuilderImpl(Modes mode) {
             this.mode = mode;
         }
         
+        @Override
         public JdbcConnectionBuilder jdbc() {
             return new JdbcConnectionBuilderImpl(this);
         }
         
+        @Override
         public String driver() {
             return driver;
         }
+        
+        @Override
         public String url() {
             return url;
         }
+        
+        @Override
         public String user() {
             return user;
         }
+        
+        @Override
         public String password() {
             return password;
+        }
+        
+        @Override
+        public int maxPoolSize() { return maxPoolSize; }
+        @Override
+        public int minPoolSize() { return minPoolSize; }
+        
+        @Override
+        public DataSource createDataSource() throws PropertyVetoException, ClassNotFoundException {
+            Class.forName(driver());
+            
+            ComboPooledDataSource source = new ComboPooledDataSource();
+            source.setDriverClass(driver());
+            source.setJdbcUrl(url());
+            source.setUser(user());
+            source.setPassword(password());
+            
+            source.setMaxPoolSize(maxPoolSize());
+            source.setMinPoolSize(minPoolSize());
+            source.setAcquireIncrement(1);
+            source.setIdleConnectionTestPeriod(300);
+            source.setMaxStatements(0);
+            
+            return source;
         }
     }
     
@@ -70,20 +112,39 @@ public class DatabaseConnections {
             this.builder = builder;
         }
         
+        @Override
         public JdbcConnectionBuilder url(String url) {
             builder.url = url;
             return this;
         }
+        
+        @Override
         public JdbcConnectionBuilder driver(String driver) {
             builder.driver = driver;
             return this;
         }
+        
+        @Override
         public JdbcConnectionBuilder user(String user) {
             builder.user = user;
             return this;
         }
+        
+        @Override
         public JdbcConnectionBuilder password(String pass) {
             builder.password = pass;
+            return this;
+        }
+        
+        @Override
+        public JdbcConnectionBuilder maxPoolSize(int max) {
+            builder.maxPoolSize = max;
+            return this;
+        }
+        
+        @Override
+        public JdbcConnectionBuilder minPoolSize(int min) {
+            builder.minPoolSize = min;
             return this;
         }
     }
