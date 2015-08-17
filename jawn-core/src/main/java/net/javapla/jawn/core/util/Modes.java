3446bb8119d4e79486dbfd5570a0937eae71e89d
MTD <alvn@alvn.dk>
2015-08-17 10:47:26 +0200

    moved Modes funtionality fully over into Modes

--- jawn-core/src/main/java/net/javapla/jawn/core/util/Modes.java
+++ jawn-core/src/main/java/net/javapla/jawn/core/util/Modes.java
@@ -22,4 +22,26 @@ public enum Modes {
         return mode;
     }
     
+    public static Modes determineModeFromSystem() {
+        String ENV = System.getProperty("JAWN_ENV") == null? System.getenv().get("JAWN_ENV"): System.getProperty("JAWN_ENV");
+        if(StringUtil.blank(ENV)){                
+            return Modes.DEV;
+//            LOGGER.warn("Environment variable JAWN_ENV not provided, defaulting to '" + ENV + "'");
+        }
+        
+        try {
+            return Modes.valueOf(ENV);
+        } catch (IllegalArgumentException e) {
+            return determineModeFromString(ENV);
+        }
+    }
+    
+    public static Modes determineModeFromString(String mode) {
+        for (Modes modes : Modes.values()) {
+            if (modes.toString().equals(mode))
+                return modes;
+        }
+        throw new IllegalArgumentException("No enum constant " + Modes.class.getName() + "." + mode);
+    }
+    
 }
