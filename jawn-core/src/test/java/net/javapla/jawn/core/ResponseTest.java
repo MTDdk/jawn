3fbc10cd4d01d15049f9d3e18acd265b88631021
MTDdk <alvn@juncle.it>
2016-08-17 10:04:13 +0200

    streamlined timing test

--- jawn-core/src/test/java/net/javapla/jawn/core/ResponseTest.java
+++ jawn-core/src/test/java/net/javapla/jawn/core/ResponseTest.java
@@ -1,59 +1,32 @@
 package net.javapla.jawn.core;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import org.junit.Test;
 
 public class ResponseTest {
 
-    @BeforeClass
-    public static void setUpBeforeClass() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownAfterClass() throws Exception {
-    }
-
-    @Before
-    public void setUp() throws Exception {
-    }
-
-    @After
-    public void tearDown() throws Exception {
-    }
-
     @Test
     public void parallisedExecution() {
-        int executionTime = 500;
-        Response resp1 = ResponseBuilder.ok().renderable(() -> {
-            System.out.println("start 1");
-            Thread.sleep(executionTime);
-            return "done 1";
-        });
+        int executionTime = 400;
         
-        Response resp2 = ResponseBuilder.ok().renderable(() -> {
-            System.out.println("start 2");
-            Thread.sleep(executionTime);
-            return "done 2";
-        });
-        
-        Response resp3 = ResponseBuilder.ok().renderable(() -> {
-            System.out.println("start 3");
-            Thread.sleep(executionTime);
-            return "done 3";
-        });
+        List<Response> responses = IntStream.range(1, 4).boxed().map(i -> 
+            ResponseBuilder.ok().renderable(() -> {
+                System.out.println("start " + i);
+                Thread.sleep(executionTime);
+                return "done " + i;
+            })
+        ).collect(Collectors.toList());
         
         long time = System.currentTimeMillis();
-        System.out.println(resp1.renderable());
-        System.out.println(resp2.renderable());
-        System.out.println(resp3.renderable());
+        responses.forEach(resp ->  System.out.println(resp.renderable()));
         time = System.currentTimeMillis() - time;
         System.out.println(time);
-        assertTrue(time < executionTime*3);
+        assertTrue(time < executionTime + (executionTime * 0.02));
     }
-
+    
 }
