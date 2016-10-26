f932d8a70758d64806c38d09459920221d4d79a2
MTDdk <alvn@juncle.it>
2016-10-26 16:25:52 +0200

    parsers now allow unquoted field names

--- jawn-core/src/main/java/net/javapla/jawn/core/parsers/ParserConfiguration.java
+++ jawn-core/src/main/java/net/javapla/jawn/core/parsers/ParserConfiguration.java
@@ -10,6 +10,7 @@ class ParserConfiguration {
     public static void config(ObjectMapper mapper) {
         mapper.configure(SerializationFeature.INDENT_OUTPUT, false);
         mapper.configure(Feature.ALLOW_COMMENTS, true);
+        mapper.configure(Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
         mapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, false);
         
         /*mapper.setDateFormat(new ISO8601DateFormat());
