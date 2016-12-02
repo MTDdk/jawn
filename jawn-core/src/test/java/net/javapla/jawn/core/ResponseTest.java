3b772ea4f47a9bd58ec369f6cf5c9c582fdb9818
MTDdk <alvn@juncle.it>
2016-12-02 14:57:30 +0100

    increase test coverage - by a minor fraction

--- jawn-core/src/test/java/net/javapla/jawn/core/ResponseTest.java
+++ jawn-core/src/test/java/net/javapla/jawn/core/ResponseTest.java
@@ -1,14 +1,31 @@
 package net.javapla.jawn.core;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.List;
+import java.util.concurrent.Future;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import org.junit.Test;
 
+
 public class ResponseTest {
+    
+    @Test
+    public void renderable() {
+        Object test = new Object();
+        Response response = ResponseBuilder.ok().renderable(test);
+        assertEquals(test, response.renderable());
+    }
+    
+    @Test
+    public void contentType() {
+        String contentType = "text/something-proprietary";
+        Response response = ResponseBuilder.ok().contentType(contentType);
+        assertEquals(contentType, response.contentType());
+    }
 
     @Test
     public void parallisedExecution() {
@@ -28,6 +45,13 @@ public class ResponseTest {
         time = System.currentTimeMillis() - time;
         System.out.println(time + " compared to " + (executionTime + executionOverhead));
         assertTrue(time < executionTime + executionOverhead);
+    }
+    
+    @Test
+    public void futureExecutionWithError() {
+        Response response = ResponseBuilder.ok().renderable(() -> {System.out.println("throwing exception"); throw new Exception();});
+        Object renderable = response.renderable();
+        assertTrue(renderable instanceof Future);
     }
     
 }
