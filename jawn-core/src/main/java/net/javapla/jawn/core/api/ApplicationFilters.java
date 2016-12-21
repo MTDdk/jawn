6b20204bdfb8cbec7874c4fd0d26a48dfba73c10
MTDdk <alvn@juncle.it>
2016-12-21 14:56:48 +0100

    extending the entry point Jawn and deprecating some api.Application* interfaces along the way

--- jawn-core/src/main/java/net/javapla/jawn/core/api/ApplicationFilters.java
+++ jawn-core/src/main/java/net/javapla/jawn/core/api/ApplicationFilters.java
@@ -1,6 +1,11 @@
 package net.javapla.jawn.core.api;
 
-
+/**
+ * 
+ * @author ann
+ * @deprecated Use Jawn#filter instead
+ */
+@Deprecated
 public interface ApplicationFilters {
 
     void filters(Filters filters);
--- jawn-core/src/main/java/net/javapla/jawn/core/api/ApplicationRoutes.java
+++ jawn-core/src/main/java/net/javapla/jawn/core/api/ApplicationRoutes.java
@@ -1,6 +1,11 @@
 package net.javapla.jawn.core.api;
 
-
+/**
+ * 
+ * @author ann
+ * @deprecated Use Jawn#get ,post, put, delete instead
+ */
+@Deprecated
 public interface ApplicationRoutes {
 
     void router(Router routes);
