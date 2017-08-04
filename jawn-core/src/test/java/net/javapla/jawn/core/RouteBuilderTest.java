c509be296db4e1d522c35d71947ffb92d815aed2
MTDdk <alvn@juncle.it>
2017-08-04 15:54:44 +0200

    mocking instead of using null

--- jawn-core/src/test/java/net/javapla/jawn/core/RouteBuilderTest.java
+++ jawn-core/src/test/java/net/javapla/jawn/core/RouteBuilderTest.java
@@ -2,11 +2,13 @@ package net.javapla.jawn.core;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
 
 import org.junit.Test;
 
 import app.controllers.UnitTestController;
 import net.javapla.jawn.core.exceptions.RouteException;
+import net.javapla.jawn.core.reflection.ActionInvoker;
 import net.javapla.jawn.core.routes.Route;
 import net.javapla.jawn.core.routes.RouteBuilder;
 
@@ -15,12 +17,12 @@ public class RouteBuilderTest {
 
     @Test(expected=RouteException.class)
     public void should_throw_because_action_not_found() {
-        RouteBuilder.get().route("/").to(UnitTestController.class, "nothing").build(new FiltersHandler(), null);
+        RouteBuilder.get().route("/").to(UnitTestController.class, "nothing").build(new FiltersHandler(), mock(ActionInvoker.class));
     }
     
     @Test
     public void should_return_route() {
-        Route route = RouteBuilder.get().to(UnitTestController.class, "simple").route("/different/path").build(new FiltersHandler(), null);
+        Route route = RouteBuilder.get().to(UnitTestController.class, "simple").route("/different/path").build(new FiltersHandler(), mock(ActionInvoker.class));
         
         assertNotNull(route);
         assertEquals("getSimple", route.getAction());
@@ -28,7 +30,7 @@ public class RouteBuilderTest {
     
     @Test
     public void should_mapToLongAction() {
-        Route route = RouteBuilder.get().to(UnitTestController.class, "longer_action").route("/different/path").build(new FiltersHandler(), null);
+        Route route = RouteBuilder.get().to(UnitTestController.class, "longer_action").route("/different/path").build(new FiltersHandler(), mock(ActionInvoker.class));
         
         assertNotNull(route);
         assertEquals("getLongerAction", route.getAction());
