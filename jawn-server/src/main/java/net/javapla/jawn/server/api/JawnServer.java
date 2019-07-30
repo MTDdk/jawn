24da717afde42dddeb9d3d6ff7e3ddf3a168f27d
MTDdk <mtd.legalus@gmail.com>
2019-07-30 11:27:14 +0200

    unused for years now

--- jawn-server/src/main/java/net/javapla/jawn/server/api/JawnServer.java
+++
@@ -1,8 +0,0 @@
-package net.javapla.jawn.server.api;
-
-import net.javapla.jawn.server.ServerConfig;
-
-public interface JawnServer {
-
-    void setupAndStartServer(ServerConfig config) throws Exception;
-}
--- jawn-server/src/test/webapp/js/moviescript.js
+++
@@ -1,5 +0,0 @@
-$.fn.editable.defaults.ajaxOptions = {type: "PUT"};
-$(document).ready(function() {
-    $('.movie-name').editable();
-    $('.movie-year').editable();
-});
