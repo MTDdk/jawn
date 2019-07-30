b6bd49ef6fe5bf67516237938524111ffd389e81
MTDdk <mtd.legalus@gmail.com>
2019-07-30 11:26:20 +0200

    implementation tests

---
+++ jawn-server-undertow/src/test/java/implementation/controllers/TestController.java
@@ -0,0 +1,28 @@
+package implementation.controllers;
+
+import com.google.inject.Inject;
+
+import net.javapla.jawn.core.Results;
+import net.javapla.jawn.core.View;
+import net.javapla.jawn.core.mvc.Body;
+import net.javapla.jawn.core.mvc.GET;
+import net.javapla.jawn.core.mvc.POST;
+import net.javapla.jawn.core.mvc.Path;
+import net.javapla.jawn.core.mvc.ViewController;
+import net.javapla.jawn.core.util.Modes;
+
+@Path("/test")
+@Path("/kage")
+@ViewController
+public class TestController {
+    
+    @Inject
+    public TestController(Modes mode) {
+        System.out.println("initialised + " + mode);
+    }
+
+    @POST
+    public void post(Double t) {
+        System.out.println("post -" + t);
+    }
+}
