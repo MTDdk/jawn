2bf92544d3e83daf87d2f5de013047795a3adfa4
MTDdk <mtd.legalus@gmail.com>
2020-07-21 13:11:56 +0200

    starting on pac4j

---
+++ jawn-security-pac4j/src/main/java/net/javapla/jawn/plugins/modules/Pac4jBootstrap.java
@@ -0,0 +1,13 @@
+package net.javapla.jawn.plugins.modules;
+
+import net.javapla.jawn.core.spi.ApplicationConfig;
+import net.javapla.jawn.core.spi.ModuleBootstrap;
+
+public class Pac4jBootstrap implements ModuleBootstrap {
+
+    @Override
+    public void bootstrap(ApplicationConfig config) {
+        
+    }
+
+}
