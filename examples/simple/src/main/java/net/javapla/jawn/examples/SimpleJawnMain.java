60ceaacfe478f2fb24013636e0a06df867fc3ff8
MTDdk <mtd.legalus@gmail.com>
2022-11-09 00:59:31 +0100

    adding a single endpoint from FrameworkBenchmark for timing testing

--- examples/simple/src/main/java/net/javapla/jawn/examples/SimpleJawnMain.java
+++ examples/simple/src/main/java/net/javapla/jawn/examples/SimpleJawnMain.java
@@ -1,6 +1,7 @@
 package net.javapla.jawn.examples;
 
 import java.io.ByteArrayInputStream;
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 
 import net.javapla.jawn.core.Context;
@@ -11,12 +12,21 @@ import net.javapla.jawn.core.Up;
 
 public class SimpleJawnMain extends Jawn {
     
+    private static final String MESSAGE = "Hello, World!";
+
+    private static final byte[] MESSAGE_BYTES = MESSAGE.getBytes(StandardCharsets.US_ASCII);
+
+    private static final ByteBuffer MESSAGE_BUFFER = (ByteBuffer) ByteBuffer
+        .allocateDirect(MESSAGE_BYTES.length)
+        .put(MESSAGE_BYTES)
+        .flip();
+    
     {
         
         get("/henning").after((ctx, result, error) -> {
             System.out.println(ctx.resp().status());
         });
-        get("/test", ctx -> ctx.resp().contentType(MediaType.HTML).respond("<html><body><h1>Workzes</h1></body</html>".getBytes()));
+        get("/html", ctx -> ctx.resp().respond("<html><body><h1>Workzes</h1></body</html>".getBytes())).produces(MediaType.HTML);
         
         get("/stream", ctx -> {
             ctx.resp().contentType(MediaType.HTML);
@@ -24,7 +34,7 @@ public class SimpleJawnMain extends Jawn {
         })
         .before(ctx -> System.out.println("streaming"))
         .before(ctx -> System.out.println("before streaming"))
-        .postResponse((ctx,error) -> System.out.println("POST"));
+        .postResponse(ctx -> System.out.println("POST"));
         
         path("/api", () -> {
             get("/v1", ctx -> {
@@ -34,13 +44,15 @@ public class SimpleJawnMain extends Jawn {
             get("/v2", ctx -> "api/v2");
         });
         
-        post("/post", ctx -> {
+        get("/plaintext", ctx -> MESSAGE_BUFFER.duplicate());//ctx -> ctx.resp().respond(MESSAGE_BUFFER.duplicate()));
+        
+        /*post("/post", ctx -> {
             System.out.println(ctx.req().body().value(StandardCharsets.UTF_8));
         });
         
         get("/json", () -> {
             return new JsonResponse("key","value");
-        }).produces(MediaType.JSON).filter(new TimingFilter());
+        }).produces(MediaType.JSON).filter(new TimingFilter());*/
     }
     
     public static record JsonResponse(String key, String value) {}
@@ -56,7 +68,7 @@ public class SimpleJawnMain extends Jawn {
         public void after(Context ctx, Object result, Throwable cause) {}
         
         @Override
-        public void onComplete(Context ctx, Throwable error) {
+        public void onComplete(Context ctx) {
             System.out.println("Timing   ::   "  + (ctx.attribute("timing").map(time -> (System.currentTimeMillis() - ((long)time))).get()));
         }
         
