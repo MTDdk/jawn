2ae2569e51cc873986ee12beeda41479741ee063
MTDdk <mtd.legalus@gmail.com>
2025-08-14 12:04:37 +0200

    extended example

--- examples/simple/src/main/java/net/javapla/jawn/examples/mvc/controllers/SimpleSingletonController.java
+++ examples/simple/src/main/java/net/javapla/jawn/examples/mvc/controllers/SimpleSingletonController.java
@@ -2,9 +2,12 @@ package net.javapla.jawn.examples.mvc.controllers;
 
 import com.typesafe.config.Config;
 
+import net.javapla.jawn.core.MediaType;
 import net.javapla.jawn.core.annotation.GET;
 import net.javapla.jawn.core.annotation.Inject;
+import net.javapla.jawn.core.annotation.PUT;
 import net.javapla.jawn.core.annotation.Path;
+import net.javapla.jawn.core.annotation.Produces;
 import net.javapla.jawn.core.annotation.Singleton;
 
 @Path("/singleton")
@@ -19,5 +22,13 @@ public class SimpleSingletonController {
     @GET
     public String text() {
         return "simple singleton";
+    }
+    
+    @PUT
+    @Path("/json")
+    @Produces(MediaType.json)
+    public Object json() {
+        record SimpleResponse(String name, int[] list) {}
+        return new SimpleResponse("collection", new int[] {12,21,37,73});
     }
 }
